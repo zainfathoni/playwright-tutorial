136734e7f83d548023bb4135fcefd4a42bb7db96
Zain Fathoni <me@zainf.dev>
2023-07-05 21:38:13 +0800

    feat(tests): add qontax.spec.ts test file

---
+++ tests/qontax.spec.ts
@@ -0,0 +1,65 @@
+import { test } from "@playwright/test";
+
+test.use({
+  storageState: "auth.local.json",
+});
+
+test("create contact by codegen", async ({ page }) => {
+  await page.goto("https://qontax.vercel.app/");
+  await page.getByRole("link", { name: "Add Contact" }).click();
+  await page.getByPlaceholder("John", { exact: true }).click();
+  await page.getByPlaceholder("John", { exact: true }).fill("Zain");
+  await page.getByPlaceholder("John", { exact: true }).press("Tab");
+  await page.getByPlaceholder("Doe", { exact: true }).fill("Fathoni");
+  await page.getByPlaceholder("https://images.unsplash.com/profile").click();
+  await page.getByRole("button", { name: "Submit" }).click();
+  await page.getByPlaceholder("https://images.unsplash.com/profile").click();
+  await page
+    .getByPlaceholder("https://images.unsplash.com/profile")
+    .fill(
+      "https://github.com/zainfathoni/www.zainfathoni.com/blob/master/static/images/zain.jpg?raw=true",
+    );
+  await page.getByPlaceholder("Web Developer").click();
+  await page.getByPlaceholder("Web Developer").fill("Senior Software Engineer");
+  await page.getByPlaceholder("Web Developer").press("Tab");
+  await page.getByPlaceholder("johndoe").fill("zainfathoni");
+  await page.getByPlaceholder("johndoe").press("Tab");
+  await page.getByPlaceholder("Your bio here").fill("Zain Fathoni");
+  await page.getByRole("button", { name: "Submit" }).click();
+});
+
+test("create contact with a better locators", async ({ page }) => {
+  await page.goto("https://qontax.vercel.app/");
+
+  await page.getByRole("link", { name: "Add Contact" }).click();
+  await page.getByLabel("Firstname").click();
+  await page.getByLabel("Firstname").fill("John");
+  await page.getByLabel("Lastname").click();
+  await page.getByLabel("Lastname").fill("Doe");
+  await page.getByLabel(/image url/i).click();
+  await page
+    .getByLabel(/image url/i)
+    .fill(
+      "https://github.com/zainfathoni/www.zainfathoni.com/blob/master/static/images/zain.jpg?raw=true",
+    );
+
+  await page.getByLabel(/occupation/i).fill("Senior Software Engineer");
+  await page.getByLabel(/twitter/i).fill("zainfathoni");
+  await page.getByLabel(/bio/i).fill("Family Man");
+
+  await page.getByRole("button", { name: "Submit" }).click();
+
+  // FIXME: await expect(page.url).not.toMatch(/new$/);
+});
+
+test("edit contact with codegen", async ({ page }) => {
+  await page
+    .getByRole("link", {
+      name: "Photo of Zain Zain Fathoni Senior Software Engineer zainfathoni",
+    })
+    .click();
+  await page.locator("section").getByRole("link").click();
+  await page.getByPlaceholder("John", { exact: true }).click();
+  await page.getByPlaceholder("John", { exact: true }).fill("Zainal");
+  await page.getByRole("button", { name: "Save" }).click();
+});
